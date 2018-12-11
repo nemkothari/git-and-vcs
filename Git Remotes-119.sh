37f49f79f2e717666b33051c8bf754606623964a
Nem kumar Kothari <32135057+nemkothari@users.noreply.github.com>
2018-12-11 09:41:33 +0530

    Add files via upload

---
+++ Git Remotes-119.sh
@@ -0,0 +1,35 @@
+## 1. Introduction to Remote Repositories ##
+
+/home/dq$ git clone /dataquest/user/git/chatbot chatbot
+
+## 2. Making Changes to Cloned Repositories ##
+
+/home/dq/chatbot$ git commit README.md -m "Updated README.md"
+
+## 3. Overview of the Master Branch ##
+
+/home/dq/chatbot$ git branch
+
+## 4. Pushing Changes to the Remote ##
+
+/home/dq/chatbot$ git push origin master
+
+## 5. Viewing Individual Commits ##
+
+/home/dq/chatbot$ git show
+
+## 6. Commits and the Working Directory ##
+
+/home/dq/chatbot$ git diff
+
+## 7. Switching to a Specific Commit ##
+
+/home/dq/chatbot$ git reset --soft 29e5bdd5dfe4519cf6c099f06ed3e4f1d66d2c8f
+
+## 8. Pulling From a Remote Repo ##
+
+/home/dq/chatbot$ git pull
+
+## 9. Referring to the Most Recent Commit ##
+
+/home/dq/chatbot$ git reset --hard 1b86adde8aae6e87e881cf1ad9b71c97210bf366
